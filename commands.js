a5b5543c89197b86adbbed6481684a6efc816247
Arnav <85503051+ArnavM16@users.noreply.github.com>
2022-06-19 13:33:52 +0530

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,76 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/forrestpknight/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/ArnavM16/";
+var email = 'mailto:forrest@fkcodes.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Arnav!👋",
+  "I'm an aspiring software developer👨‍💻, who builds engaging websites like this one.",
+  "I have distractingly many intrest",
+  "Currently learning The Rust🦀 programming language.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        Your favorite anon on CT' + '</a>',
+  'linkedin       I\'m GenZ',
+  'instagram      Don\'t wanna disturb my mental health!',
+  'github         <a href="' + github + '" target="_blank">github/ArnavM16' + "</a>",
+  "<br>"
+];
+
+
+projects = [
+  "<br>",
+  "Still curating... most projects are on localhost, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Arnav?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">secret</span>         Find the password',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Arnav Mohod (AM) Not A Corporation. All Rights reserved.</span>',
+  "         ___   ____",
+  "       /' --;^/ ,-_\\     \\ | /       ",
+  "      / / --o\\ o-\\ \\\\   --(_)--   ",
+  "     /-/-/|o|-|\\-\\\\|\\\\   / | \\   ______                          __     __ __       _       __    __   ",
+  "      '`  ` |-|   `` '          / ____/___  _____________  _____/ /_   / //_/____  (_)___ _/ /_  / /_ ",
+  "            |-|                / /_  / __ \\/ ___/ ___/ _ \\/ ___/ __/  / ,<  / __ \\/ / __ `/ __ \\/ __/ ",
+  "            |-|O              / __/ / /_/ / /  / /  /  __(__  ) /_   / /| |/ / / / / /_/ / / / / /_  ",
+  "            |-(\\,__          /_/    \\____/_/  /_/   \\___/____/\\__/  /_/ |_/_/ /_/_/\\__, /_/ /_/\\__/ ",
+  "         ...|-|\\--,\\_....                                                         /____/    © 2022",
+  "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
+  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+secret = [
+    "<br>",
+    '<span class="command">sudo</span>           Only use if you\'re admin',
+    "<br>"
+  ];
